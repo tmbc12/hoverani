9432d8b873e530a47ebd69652ad0fa6708182254
tmbc12 <reachus@tmbc.in>
2024-04-05 16:13:54 +0530

    changed logo

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ tex => {
   texture.minFilter = THREE.LinearFilter;
 
   loader.load(
-  'https://i.ibb.co/Cv5cSz1/Frame-25.png',
+  'https://i.ibb.co/3Wj2D3R/Frame-427322079-1.png',
   tex => {
     _500 = tex;
 
