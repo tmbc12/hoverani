8a87df703ec8c41a8d8a814d7a476d02c852c515
tmbc12 <reachus@tmbc.in>
2024-04-05 18:35:45 +0530

    /

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ tex => {
   texture.minFilter = THREE.LinearFilter;
 
   loader.load(
-  'https://i.ibb.co/3Wj2D3R/Frame-427322079-1.png',
+  'https://i.ibb.co/XpK1hQY/Frame-427322079-1.png',
   tex => {
     _500 = tex;
 
